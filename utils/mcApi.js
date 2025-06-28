4a985c0073cd974cfb4d1fb8779e99c39fe8f367
Programmer-Timmy <tim.vanderkloet@gmail.com>
2025-06-28 18:54:14 +0200

    Implement whitelist management commands and improve error handling

--- utils/mcApi.js
+++ utils/mcApi.js
@@ -1,8 +1,11 @@
 
-export async function getMinecraftUUID(username) {
+async function getMinecraftUUID(username) {
     const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
     if (!res.ok) return null;
     const data = await res.json();
     return data.id || null;
 }
 
+module.exports = {
+    getMinecraftUUID
+}
